554a74ec6495d5b699e4e96b6c5f35012cf31782
Jeandra <jeandra8789@gmail.com>
2022-07-06 23:39:17 -0300

    add css e js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,29 @@
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Load light or dark mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+    if(darkMode) {
+        toggleDarkMode();
+    }  
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    //Save or remove dark mode from localStorage
+    localStorage.removeItem("dark");
+
+    if (document.body.classList.container("dark")) {
+        localStorage.setItem("dark", 1)
+    }
+});
 
